b07955bd10d4db9cf9fe2c6d76adf3b0823f25ca
Louise Deeth <louisedeeth@gmail.com>
2025-04-20 16:01:52 +0100

    Removed doubled up code from config

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { environment } from './environments/environment';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-  ],
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
 
