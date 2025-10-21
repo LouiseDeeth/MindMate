c8414a7b79d388de016e1a366c6f10af1c50d7da
Louise Deeth <louisedeeth@gmail.com>
2025-10-22 00:26:10 +0100

    Bug fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   backendUrl: 'https://claudeproxy-3p3tk5tdca-uc.a.run.app',
   firebase: {
-    apiKey: 'FIREBASE_API_KEY',
+    apiKey: 'AIzaSyBowZps7leZxIRkw_RJ1bcrBPFVXvU_7-0',
     authDomain: 'mindmate-cc00b.firebaseapp.com',
     projectId: 'mindmate-cc00b',
     storageBucket: 'mindmate-cc00b.appspot.com',
